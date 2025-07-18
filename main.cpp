e5306874e267798bfb6d27495fbb2e431c4b36a5
YonYon2 <yonathantorr@gmail.com>
2025-07-18 13:35:55 -0400

    Added printing of contents in passed filename

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+/* procedure:
+ - check if parameter is given
+ - check filename exists
+ - open file
+ - view contents between a range
+ - quit by pressing File > Quit
+*/
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        std::cout << "Nothin, pass filename as parameter!\n";
+        return -1;
+    }
+    std::fstream dump(argv[1]);
+    if (!dump) {
+        std::cout << "File does not exist!\n";
+        return -1;
+    }
+    char current;
+    int pos = 0;
+    while (dump.get(current)) {
+        std::cout << +current << ' ';
+        if (++pos >= 16){
+            std::cout << '\n';
+            pos = 0;
+        }
+    }
     return 0;
 }
